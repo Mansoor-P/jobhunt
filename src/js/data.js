1dc391a23d0163e338efc50302a87309b260e723
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-05-28 09:30:01 +0530

    scripts

--- src/js/data.js
+++ src/js/data.js
@@ -1,32 +1,33 @@
 
 
-    // Dummy job data
-    var jobs = [
-      {
-        title: 'Frontend Developer',
-        company: 'ABC Inc.',
-        location: 'New York',
-        salary: '$80,000',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id lectus quis quam facilisis tristique. Nullam vel purus sed odio dapibus efficitur et sed metus. Curabitur pharetra lectus ac diam consequat, at scelerisque mi tincidunt.',
-        logo: './img/about-1.jpg',
-        qualifications: 'Bachelor\'s degree in Computer Science or related field',
-        skills: 'HTML, CSS, JavaScript',
-        responsibilities: 'Frontend development and maintenance',
-        applyLink: 'https://www.firstnaukri.com/careers/customised/landingpage/hormann/index.html' // Update the URL to include the job ID as a parameter
-      },
-      {
-        title: 'Backend Developer',
-        company: 'XYZ Corp.',
-        location: 'San Francisco',
-        salary: '$100,000',
-        description: 'Earn money online – it is easy with MetroOpinioEarning money online is quite easy with surveys from MetroOpinion.We have optimized our software to find the most relevant surveys. More relevant surveys means more money for you.When we find a perfect survey for you, we will send you an invitation via email. It is important that you sign up with your primary email address. Registering with the email address you use regularly is the best way to earn money. If you do not participate when invited, the invitation will go to another member of MetroOpinion.Payment is easy with MetroOpinion. You can choose between multiple payment options. We have thousands of happy members of MetroOpinion that have received money from us.It is fun to earn money with MetroOpinion. Companies use your answers to create better products. This means your answers make a difference.',
-        logo: './img/about-2.jpg',
-        qualifications: 'Bachelor\'s degree in Computer Science or related field',
-        skills: 'Python, SQL, RESTful API',
-        responsibilities: 'Backend development and database management',
-        applyLink: 'https://arwizon.zohorecruit.com/jobs/Careers/573338000005666019/Software-Developer-Trainee' // Update the URL to include the job ID as a parameter
-      },
-      {
+// Dummy job data
+var jobs = [
+  
+  {
+    title: 'java developer',
+    company: 'HCL tech',
+    location: 'banglore || hydrabad',
+    salary: '10 LPA',
+    description: ' Back End Developer,Industry Type: IT Services & Consulting,Department: Engineering - Software & QA,Employment Type: Full Time, PermanentRole Category: Software DevelopmentEducationUG: Any GraduateKey SkillsSkills highlighted with ‘‘ are preferred keyskillsspring bootjavaspring mvcweb technologiesweb server',
+    logo: './img/companies/hcl/hcl.jpg',
+    qualifications: 'Bachelor\'s degree in Computer Science or related field',
+    skills: ' Core Java 8 concepts, Spring Core, Spring MVC, Spring Boot, REST API, JSON, XML, SQL, Web Server ',
+    responsibilities: ' Back End Developer,Industry Type: IT Services & Consulting,Department: Engineering - Software & QA,Employment Type: Full Time, PermanentRole Category: Software DevelopmentEducationUG: Any GraduateKey SkillsSkills highlighted with ‘‘ are preferred keyskillsspring bootjavaspring mvcweb technologiesweb server',
+    applyLink: 'https://www.naukri.com/job-listings-java-developer-5-8-years-joiner-bangalore-hyderabad-hcltech-hyderabad-secunderabad-bangalore-bengaluru-5-to-8-years-260323001640'
+  },
+  {
+    title: 'WIPRO MEGA WALKIN DRIVE: HYDERABAD',
+    company: 'Wipro',
+    location: 'Hydrabad',
+    salary: '3.5 LPA',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id lectus quis quam facilisis tristique. Nullam vel purus sed odio dapibus efficitur et sed metus. Curabitur pharetra lectus ac diam consequat, at scelerisque mi tincidunt.',
+    logo: './img/companies/wipro/wipro.png',
+    qualifications: 'Bachelor\'s degree in Computer Science or related field',
+    skills: 'HTML, CSS, JavaScript',
+    responsibilities: 'Frontend development and maintenance',
+    applyLink: 'https://frontlinesmedia.in/wipro-mega-walkin-drive-hyderabad/'
+  },
+  {
     title: 'Full Stack Developer',
     company: 'Tech Solutions',
     location: 'Seattle',
@@ -36,103 +37,103 @@
     qualifications: 'Bachelor\'s degree in Computer Science or related field',
     skills: 'HTML, CSS, JavaScript, Python',
     responsibilities: 'Full stack development and system architecture',
-    applyLink: 'job_details.html?id=2' // Update the URL to include the job ID as a parameter
+    applyLink: ''
   },
   {
     title: 'UI/UX Designer',
     company: 'Design Co.',
-    location: 'Los Angeles',
+    location: 'Tokyo',
     salary: '$70,000',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id lectus quis quam facilisis tristique. Nullam vel purus sed odio dapibus efficitur et sed metus. Curabitur pharetra lectus ac diam consequat, at scelerisque mi tincidunt.',
     logo: './img/about-4.jpg',
     qualifications: 'Bachelor\'s degree in Graphic Design or related field',
     skills: 'Adobe Photoshop, Sketch, Wireframing',
     responsibilities: 'UI/UX design and prototyping',
-    applyLink: 'job_details.html?id=3' // Update the URL to include the job ID as a parameter
+    applyLink: ''
   },
-  {
-    title: 'Data Analyst',
-    company: 'Analytics Inc.',
-    location: 'Chicago',
-    salary: '$70,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_4.jpg',
-    qualifications: 'Bachelor\'s degree in Statistics or related field',
-    skills: 'Data analysis, SQL, Excel',
-    responsibilities: 'Analyzing and interpreting complex data sets',
-    applyLink: 'job_details.html?id=3'
-  },
-  {
-    title: 'Marketing Manager',
-    company: 'Growth Agency',
-    location: 'Los Angeles',
-    salary: '$90,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_5.jpg',
-    qualifications: 'Bachelor\'s degree in Marketing or related field',
-    skills: 'Digital marketing, SEO, Social media management',
-    responsibilities: 'Developing and implementing marketing strategies',
-    applyLink: 'job_details.html?id=4'
-  },
-  {
-    title: 'Product Manager',
-    company: 'Tech Startup',
-    location: 'Austin',
-    salary: '$95,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_6.jpg',
-    qualifications: 'Bachelor\'s degree in Business or related field',
-    skills: 'Product management, Agile methodologies',
-    responsibilities: 'Defining product roadmap and leading cross-functional teams',
-    applyLink: 'job_details.html?id=5'
-  },
-  {
-    title: 'Sales Executive',
-    company: 'Sales Solutions',
-    location: 'Boston',
-    salary: '$85,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_7.jpg',
-    qualifications: 'Bachelor\'s degree in Business or related field',
-    skills: 'Sales, Negotiation, Relationship management',
-    responsibilities: 'Driving sales and meeting revenue targets',
-    applyLink: 'job_details.html?id=6'
-  },
-  {
-    title: 'Graphic Designer',
-    company: 'Creative Studio',
-    location: 'Miami',
-    salary: '$60,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_8.jpg',
-    qualifications: 'Bachelor\'s degree in Design or related field',
-    skills: 'Adobe Creative Suite, Typography, Illustration',
-    responsibilities: 'Creating visual assets for various media channels',
-    applyLink: 'job_details.html?id=7'
-  },
-  {
-    title: 'Human Resources Manager',
-    company: 'HR Solutions',
-    location: 'Houston',
-    salary: '$80,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_9.jpg',
-    qualifications: 'Bachelor\'s degree in Human Resources or related field',
-    skills: 'Recruitment, Employee relations, HR policies',
-    responsibilities: 'Overseeing HR functions and managing employee relations',
-    applyLink: 'job_details.html?id=8'
-  },
-  {
-    title: 'Financial Analyst',
-    company: 'Finance Inc.',
-    location: 'Dallas',
-    salary: '$75,000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    logo: 'company_10.jpg',
-    qualifications: 'Bachelor\'s degree in Finance or related field',
-    skills: 'Financial analysis, Excel, Data visualization',
-    responsibilities: 'Analyzing financial data and preparing reports',
-    applyLink: 'job_details.html?id=9'
-  }
-      // Add more dummy jobs here...
-    ];
+  // {
+  //   title: 'Data Analyst',
+  //   company: 'Analytics Inc.',
+  //   location: 'Chicago',
+  //   salary: '$70,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_4.jpg',
+  //   qualifications: 'Bachelor\'s degree in Statistics or related field',
+  //   skills: 'Data analysis, SQL, Excel',
+  //   responsibilities: 'Analyzing and interpreting complex data sets',
+  //   applyLink: 'job_details.html?id=3'
+  // },
+  // {
+  //   title: 'Marketing Manager',
+  //   company: 'Growth Agency',
+  //   location: 'Los Angeles',
+  //   salary: '$90,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_5.jpg',
+  //   qualifications: 'Bachelor\'s degree in Marketing or related field',
+  //   skills: 'Digital marketing, SEO, Social media management',
+  //   responsibilities: 'Developing and implementing marketing strategies',
+  //   applyLink: 'job_details.html?id=4'
+  // },
+  // {
+  //   title: 'Product Manager',
+  //   company: 'Tech Startup',
+  //   location: 'Austin',
+  //   salary: '$95,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_6.jpg',
+  //   qualifications: 'Bachelor\'s degree in Business or related field',
+  //   skills: 'Product management, Agile methodologies',
+  //   responsibilities: 'Defining product roadmap and leading cross-functional teams',
+  //   applyLink: 'job_details.html?id=5'
+  // },
+  // {
+  //   title: 'Sales Executive',
+  //   company: 'Sales Solutions',
+  //   location: 'Boston',
+  //   salary: '$85,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_7.jpg',
+  //   qualifications: 'Bachelor\'s degree in Business or related field',
+  //   skills: 'Sales, Negotiation, Relationship management',
+  //   responsibilities: 'Driving sales and meeting revenue targets',
+  //   applyLink: 'job_details.html?id=6'
+  // },
+  // {
+  //   title: 'Graphic Designer',
+  //   company: 'Creative Studio',
+  //   location: 'Miami',
+  //   salary: '$60,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_8.jpg',
+  //   qualifications: 'Bachelor\'s degree in Design or related field',
+  //   skills: 'Adobe Creative Suite, Typography, Illustration',
+  //   responsibilities: 'Creating visual assets for various media channels',
+  //   applyLink: 'job_details.html?id=7'
+  // },
+  // {
+  //   title: 'Human Resources Manager',
+  //   company: 'HR Solutions',
+  //   location: 'Houston',
+  //   salary: '$80,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_9.jpg',
+  //   qualifications: 'Bachelor\'s degree in Human Resources or related field',
+  //   skills: 'Recruitment, Employee relations, HR policies',
+  //   responsibilities: 'Overseeing HR functions and managing employee relations',
+  //   applyLink: 'job_details.html?id=8'
+  // },
+  // {
+  //   title: 'Financial Analyst',
+  //   company: 'Finance Inc.',
+  //   location: 'Dallas',
+  //   salary: '$75,000',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //   logo: 'company_10.jpg',
+  //   qualifications: 'Bachelor\'s degree in Finance or related field',
+  //   skills: 'Financial analysis, Excel, Data visualization',
+  //   responsibilities: 'Analyzing financial data and preparing reports',
+  //   applyLink: 'job_details.html?id=9'
+  // }
+  // Add more dummy jobs here...
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,29 @@
-function toggleTheme() {
-    const themeSwitcher = document.getElementById('themeSwitcher');
-    const body = document.body;
+$(document).ready(function () {
+    var jobNames = $(".job-names .job-name");
+    var currentIndex = 0;
 
-    if (themeSwitcher.checked) {
-        body.classList.add('dark-theme');
-    } else {
-        body.classList.remove('dark-theme');
+    // Show initial job names
+    showJobNames();
+
+    // Previous arrow click event
+    $(".prev-arrow").click(function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showJobNames();
+        }
+    });
+
+    // Next arrow click event
+    $(".next-arrow").click(function () {
+        if (currentIndex < jobNames.length - 1) {
+            currentIndex++;
+            showJobNames();
+        }
+    });
+
+    // Function to display the relevant job names
+    function showJobNames() {
+        jobNames.hide();
+        jobNames.eq(currentIndex).show();
     }
-}
-// get search bar and filter dropdowns
-const searchBar = document.querySelector('.search-bar input[type="text"]');
-const locationDropdown = document.querySelector('.filter-box select:nth-of-type(1)');
-const categoryDropdown = document.querySelector('.filter-box select:nth-of-type(2)');
-const experienceDropdown = document.querySelector('.filter-box select:nth-of-type(3)');
-
-// get job listings table and rows
-const jobListingsTable = document.querySelector('table');
-const jobListingsRows = jobListingsTable.querySelectorAll('tbody tr');
-
-// add event listeners to search bar and filter dropdowns
-searchBar.addEventListener('keyup', filterJobListings);
-locationDropdown.addEventListener('change', filterJobListings);
-categoryDropdown.addEventListener('change', filterJobListings);
-experienceDropdown.addEventListener('change', filterJobListings);
-
-function filterJobListings() {
-const searchText = searchBar.value.toLowerCase();
-const locationFilter = locationDropdown.value.toLowerCase();
-const categoryFilter = categoryDropdown.value.toLowerCase();
-const experienceFilter = experienceDropdown.value.toLowerCase();
-
-// loop through each job listing row
-jobListingsRows.forEach(row => {
-const jobTitle = row.querySelector('td:nth-of-type(1)').textContent.toLowerCase();
-const location = row.querySelector('td:nth-of-type(2)').textContent.toLowerCase();
-const category = row.querySelector('td:nth-of-type(3)').textContent.toLowerCase();
-const experience = row.querySelector('td:nth-of-type(4)').textContent.toLowerCase();
-
-// hide row if it doesn't match the search and filter criteria
-if (!jobTitle.includes(searchText)
-  || (locationFilter !== '' && location !== locationFilter)
-  || (categoryFilter !== '' && category !== categoryFilter)
-  || (experienceFilter !== '' && experience !== experienceFilter)) {
-  row.style.display = 'none';
-} else {
-  row.style.display = '';
-}
 });
-}
