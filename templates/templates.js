54b94ed8947954ed5eb343b15211bf17468534d7
MunafVlog <munaf6007@gmail.com>
2023-09-07 13:15:50 +0530

    code templates introdused

---
+++ templates/templates.js
@@ -0,0 +1,25 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Load the navbar using JavaScript
+    const navbarContainer = document.getElementById('navbarContainer');
+    fetch('templates/nav/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            navbarContainer.innerHTML = data;
+        });
+        // Load the marquee using JavaScript
+        const marqueeContainer = document.getElementById('marqueeContainer');
+        fetch('templates/marquee/marquee.html')
+            .then(response => response.text())
+            .then(data => {
+                marqueeContainer.innerHTML = data;
+            });
+            // Load the footer using JavaScript
+    const footerContainer = document.getElementById('footerContainer');
+    fetch('templates/footer/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            footerContainer.innerHTML = data;
+        });
+})
