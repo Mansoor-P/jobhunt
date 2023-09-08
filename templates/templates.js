0e2ed7c90e72e8b057f2c7cd9e5d16c4a13baac1
MunafVlog <munaf6007@gmail.com>
2023-09-08 13:05:47 +0530

    updated

--- templates/templates.js
+++ templates/templates.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', function () {
     // Load the navbar using JavaScript
     const navbarContainer = document.getElementById('navbarContainer');
@@ -8,18 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             navbarContainer.innerHTML = data;
         });
-        // Load the marquee using JavaScript
-        const marqueeContainer = document.getElementById('marqueeContainer');
-        fetch('templates/marquee/marquee.html')
-            .then(response => response.text())
-            .then(data => {
-                marqueeContainer.innerHTML = data;
-            });
-            // Load the footer using JavaScript
+
+    // Load the category using JavaScript
+    const categoryContainer = document.getElementById('categoryContainer');
+    fetch('templates/category/category.html')
+        .then(response => response.text())
+        .then(data => {
+            categoryContainer.innerHTML = data;
+        });
+
+    // Load the marquee using JavaScript
+    const marqueeContainer = document.getElementById('marqueeContainer');
+    fetch('templates/marquee/marquee.html')
+        .then(response => response.text())
+        .then(data => {
+            marqueeContainer.innerHTML = data;
+        });
+    // Load the footer using JavaScript
     const footerContainer = document.getElementById('footerContainer');
     fetch('templates/footer/footer.html')
         .then(response => response.text())
         .then(data => {
             footerContainer.innerHTML = data;
         });
-})
+});
