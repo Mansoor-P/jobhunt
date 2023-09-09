08b468697a3ba179e7ef0194c6087079440ff0fd
MunafVlog <munaf6007@gmail.com>
2023-09-09 15:20:47 +0530

    templates upadates

--- templates/templates.js
+++ templates/templates.js
@@ -2,21 +2,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Load the navbar using JavaScript
     const navbarContainer = document.getElementById('navbarContainer');
-    fetch('templates/nav/navbar.html')
+    fetch('./templates/nav/navbar.html')
         .then(response => response.text())
         .then(data => {
             navbarContainer.innerHTML = data;
         });
     // Load the searchingNavContainer using JavaScript
     const searchingNavContainer = document.getElementById('searchingNavContainer');
-    fetch('/mj-career/templates/searchingnav/searchingNav.html')
+    fetch('./templates/searchingnav/searchingNav.html')
         .then(response => response.text())
         .then(data => {
             searchingNavContainer.innerHTML = data;
         });
     // Load the carouselContainer using JavaScript
     const carouselContainer = document.getElementById('carouselContainer');
-    fetch('/mj-career/templates/carousel/carousel.html')
+    fetch('./templates/carousel/carousel.html')
         .then(response => response.text())
         .then(data => {
             carouselContainer.innerHTML = data;
@@ -24,22 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Load the category using JavaScript
     const categoryContainer = document.getElementById('categoryContainer');
-    fetch('/mj-career/templates/category/category.html')
+    fetch('./templates/category/category.html')
         .then(response => response.text())
         .then(data => {
             categoryContainer.innerHTML = data;
         });
 
-    // Load the marquee using JavaScript
-    const marqueeContainer = document.getElementById('marqueeContainer');
-    fetch('templates/marquee/marquee.html')
-        .then(response => response.text())
-        .then(data => {
-            marqueeContainer.innerHTML = data;
-        });
         // Load the marquee using JavaScript
     const latestJobsContainer = document.getElementById('latestJobsContainer');
-    fetch('templates/latest/latestjobs.html')
+    fetch('./templates/latest/latestjobs.html')
         .then(response => response.text())
         .then(data => {
             latestJobsContainer.innerHTML = data;
@@ -47,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // Load the footer using JavaScript
     const footerContainer = document.getElementById('footerContainer');
-    fetch('templates/footer/footer.html')
+    fetch('./templates/footer/footer.html')
         .then(response => response.text())
         .then(data => {
             footerContainer.innerHTML = data;
