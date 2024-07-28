a71e0d84eebdcf8d98b87e9ce61c68af9c2c374d
Mansoor-P <munaf6007@gmail.com>
2024-07-28 12:25:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import JobList from "./pages/JobList";
+import ErrorPage from "./pages/ErrorPage";
+import "./assets/styles/App.css";
+
 function App() {
   return (
-    <>
-      <h1>MJ Career</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
