5c414f2a0458959f1b70a0cc860b8d7d4edf7a30
Mansoor-P <munaf6007@gmail.com>
2024-07-30 12:58:41 +0530

    images imported

--- src/assets/images/companies.js
+++ src/assets/images/companies.js
@@ -1,10 +1,9 @@
 const companies = [
   { name: 'Google', logo: 'src/assets/images/logos/google.png' },
-  { name: 'Facebook', logo: 'src/assets/images/logos/facebook.png' },
+  { name: 'Hibspot', logo: 'src/assets/images/logos/hubspot.png' },
   { name: 'FedEx', logo: 'src/assets/images/logos/fedex.png' },
   { name: 'Airbnb', logo: 'src/assets/images/logos/airbnb.png' },
   { name: 'Microsoft', logo: 'src/assets/images/logos/microsoft.png' },
-  { name: 'Slack', logo: 'src/assets/images/logos/slack.png' },
   { name: 'Walmart', logo: 'src/assets/images/logos/walmart.png' },
 ];
 
