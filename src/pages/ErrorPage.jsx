746bc8a19a55e62699eae074de22213595aee36e
Mansoor-P <munaf6007@gmail.com>
2024-07-30 12:58:21 +0530

    error page and styles

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,14 +4,28 @@ import "../assets/styles/ErrorPage.css";
 
 function ErrorPage() {
   return (
-    <div className="error-page">
+    <>
       <Header />
-      <main>
-        <h2>404 - Page Not Found</h2>
-        <p>The page you are looking for does not exist.</p>
-      </main>
+
+      <div className="error-page">
+        <main>
+          <h2>Oops! 404 - Page Not Found</h2>
+          <p>
+            We could not find the page you were looking for. It might have been
+            moved or deleted.
+          </p>
+          <p>
+            Don not worry, you can go back to our <a href="/">homepage</a> or
+            check out our latest updates.
+          </p>
+          <img src="../../src/assets/images/404.png" alt="404 Error" />
+          <button onClick={() => (window.location.href = "/")}>
+            Go to Homepage
+          </button>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
