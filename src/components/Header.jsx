9b44f3e0ed503e6f4856ce5c56395a16d3b863bd
Mansoor-P <munaf6007@gmail.com>
2024-07-30 12:56:44 +0530

    header component and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import "../assets/styles/Header.css";
 
 function Header() {
   return (
-    <header className="header">
-      <h1>Job Portal</h1>
-      <nav>
+    <header className="container header">
+      <h1>Jobhunt</h1>
+      <nav className="nav">
         <a href="/">Home</a>
         <a href="/jobs">Job Listings</a>
-        <a href="/contact">Contact</a>
+        <a href="/about">About Us</a>
+        <button>Blogs</button>
       </nav>
     </header>
   );
