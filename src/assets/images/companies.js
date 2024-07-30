363d8dc6b73c4e1fe432ea2073732defcf348b43
Mansoor-P <munaf6007@gmail.com>
2024-07-30 08:20:10 +0530

    all companies imported

---
+++ src/assets/images/companies.js
@@ -0,0 +1,11 @@
+const companies = [
+  { name: 'Google', logo: 'src/assets/images/logos/google.png' },
+  { name: 'Facebook', logo: 'src/assets/images/logos/facebook.png' },
+  { name: 'FedEx', logo: 'src/assets/images/logos/fedex.png' },
+  { name: 'Airbnb', logo: 'src/assets/images/logos/airbnb.png' },
+  { name: 'Microsoft', logo: 'src/assets/images/logos/microsoft.png' },
+  { name: 'Slack', logo: 'src/assets/images/logos/slack.png' },
+  { name: 'Walmart', logo: 'src/assets/images/logos/walmart.png' },
+];
+
+export default companies;
