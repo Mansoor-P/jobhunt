cc90c3757bcd258e156e321bd8be50436ce28e11
Mansoor-P <munaf6007@gmail.com>
2024-07-30 17:43:08 +0530

    jobs ui and data updated

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import "../assets/styles/JobCard.css";
+import { jobs } from "../data/jobs";
 
 function JobCard() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const jobsPerPage = 5;
+
+  const totalPages = Math.ceil(jobs.length / jobsPerPage);
+
+  const indexOfLastJob = currentPage * jobsPerPage;
+  const indexOfFirstJob = indexOfLastJob - jobsPerPage;
+  const currentJobs = jobs.slice(indexOfFirstJob, indexOfLastJob);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div>
-      <JobCard />
+      <ul className="job-card-list">
+        {currentJobs.map((job, index) => (
+          <li key={index} className="job-card-item">
+            <h3>{job.title}</h3>
+            <div className="cat-com">
+              <p className="category">{job.category}</p>
+              <p>{job.company}</p>
+            </div>
+            <span>
+              <i className="bi bi-geo-alt-fill"></i>
+              {job.location}
+            </span>
+            <a href={job.applyLink} target="_blank" rel="noopener noreferrer">
+              More Info
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          {"<"}
+        </button>
+        <span>
+          {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          {">"}
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,11 @@
+import JobCard from "./JobCard";
 import "../assets/styles/JobListings.css";
+import { jobs } from "../data/jobs";
 
 function JobListings() {
   return (
     <div className="job-listings">
-      <p>No job listings available.</p>
+      {jobs.length > 0 ? <JobCard /> : <p>No job listings available.</p>}
     </div>
   );
 }
