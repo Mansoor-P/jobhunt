8056f617787460ea524c6d0e4bc1578c953e02c8
Mansoor-P <munaf6007@gmail.com>
2024-08-17 00:20:52 +0530

    Refactor: Enhance header styles for better mobile compatibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,62 @@
-import "../assets/styles/Header.css";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useTheme } from "../context/ThemeContext";
+import { FaMoon, FaSun } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/styles/Header.css";
 
 function Header() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <header className="header">
-      <h1>
-        <Link to="/">Job Hunt</Link>
-      </h1>
-      <nav className="nav">
-        <Link to="/jobs">Job Listings</Link>
-        <Link to="/about">About Us</Link>
-        <button onClick={() => window.location.href = '/blogs'}>Blogs</button>
+    <header className={` header ${theme}`}>
+      <nav
+        className={`navbar navbar-expand-lg ${
+          theme === "dark" ? "navbar-dark bg-dark" : "navbar-light bg-light"
+        }`}
+      >
+        <Link to="/" className="navbar-brand header-logo" style={{color:"#4f46e5"}}>
+          Job Hunt
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/jobs" className="nav-link">
+                Job Listings
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/blogs" className="nav-link">
+                Blogs
+              </Link>
+            </li>
+            <li className="nav-item">
+              <button
+                className="btn theme-toggle"
+                onClick={toggleTheme}
+                aria-label="Toggle theme"
+              >
+                {theme === "dark" ? <FaSun /> : <FaMoon />}
+              </button>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
